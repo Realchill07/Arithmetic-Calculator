03258bb57fae645df088c5e25d025f98d517619b
Lakshya Lakhotia <lakshyalakhotia2@gmail.com>
2025-08-12 01:38:10 +0530

    Added sqaure root and power of a number

--- Calculator.c
+++ Calculator.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
     int x;
     float a;
     float b;
+    float c;
+    float d;
+    float e;
     float sum;
     float sub;
     float multiply;
     float divide;
     float percentage;
-    printf("This is a calculator for operations on two numbers[Calculates till 6 decimals :)] .Choose\n 1 for Addition\n 2 for Subtraction\n 3 for Multiplication\n 4 for Divide\n 5 for percentage\n");
+    float root;
+    float power;
+    printf("This is a calculator for operations on two numbers[Calculates till 6 decimals :)] .Choose\n 1 for Addition\n 2 for Subtraction\n 3 for Multiplication\n 4 for Divide\n 5 for percentage\n 6 for sqaure root\n 7 for power of a number\n");
     scanf("%d",&x);
+    if (x == 6) {
+        printf("Input the number : ");
+        scanf("%f",&c);
+    }
+    else if (x == 7) {
+        printf("Input the base : ");
+        scanf("%f",&d);
+        printf("Input the power : ");
+        scanf("%f",&e);
+    }
+    else {
     printf("\n Input first number : ");
     scanf("%f",&a);
     printf("\n Input second number : ");
     scanf("%f",&b);
+    }
     if (x==1) {
         sum = a+b;
         printf("Sum of %f and %f is : %f",a,b,sum);
@@ -33,6 +51,14 @@ int main(){
     else if (x==5) {
         percentage = (a / b) * 100;
         printf("%f is %f percent of %f",a,percentage,b);
+    }
+    else if (x==6){
+        root = sqrt(c);
+        printf("Square root of %f is %f",c,root);
+    }
+    else if (x==7){
+        power = pow(d,e);
+        printf("%f to the power of %f is : %f",d,e,power);
     }
     else {
         printf("Please write a number from the given options and rerun the application");
